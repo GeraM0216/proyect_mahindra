5dfa2265155af1f012b6c6bbdf21a3a903c3e0c7
Gerardo Molina <gmoli2002@gmail.com>
2025-02-28 21:06:44 -0600

    login agregado

---
+++ resources/views/login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+    <link rel="stylesheet" href="styles1.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <form action="search.html ">
+            <h1>Sign In</h1>
+            <div class="input-box">
+                <input type="text" placeholder="email@techmahindra.org"required>
+                <i class='bx bxs-user'></i>
+            </div>
+
+            <div class="input-box">
+                <input type="password" placeholder="Password"required>
+                <i class='bx bxs-lock'></i>
+            </div>
+
+            <div class="remember-forgot">
+                <label><input type="checkbox"> Remember me</label>
+                <a href="a">Forgot password?</a>
+            </div>
+
+            <div class="login">
+                
+                <button type="submit" class="btn">Login</button>
+            </div>
+
+            <div class="register-link">
+                <p>Dont have an account?<a href="#">Register</a></p>
+            </div>
+
+            <div class="terms">
+                <p>By clicking continue, you agree to our<a href="#"> Terms of Service</a> and <a href="#">Privacy Policy</a></p>
+            </div>
+        </form>
+
+
+    </div>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OpenAIController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/search', function () {
+    return view('search');
 });
 
-Route::get('/inicio', function () {
-    return view('starting_page');
+Route::get('/login', function () {
+    return view('login');
 });
 
 Route::get('chat', [OpenAIController::class,'index']);
